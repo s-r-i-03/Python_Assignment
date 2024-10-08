eeab360ad0c1134117cc7c45f6977d826a38a60c
s-r-i-03 <145031870+s-r-i-03@users.noreply.github.com>
2024-10-08 21:08:00 +0530

    Create 3.py

---
+++ Input Output and Arithmatic Operations/3.py
@@ -0,0 +1,9 @@
+sub1=int(input("Enter the marks of sub1:"))
+sub2=int(input("Enter the marks of sub2:"))
+sub3=int(input("Enter the marks of sub3:"))
+sub4=int(input("Enter the marks of sub4:"))
+sub5=int(input("Enter the marks of sub5:"))
+agg= (sub1+sub2+sub3+sub4+sub5)
+print("The aggregate marks is:",agg)
+per= (agg/500)*100
+print("The percentage obtained:",per)
