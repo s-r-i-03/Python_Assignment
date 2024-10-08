ba354385f8d12f52c4e84831e5d669d5a414fdee
s-r-i-03 <145031870+s-r-i-03@users.noreply.github.com>
2024-10-08 21:13:51 +0530

    #5

---
+++ Input Output and Arithmatic Operations/5.py
@@ -0,0 +1,12 @@
+len=float(input("Enter the length of rectangle:"))
+bred=float(input("Enter the breadth of rectangle:"))
+rad=float(input("Enter the radius of circle:"))
+rec_area= len*bred
+rec_per= 2*(len + bred)
+print("Area of rectangle", rec_area)
+print("Perimeter of rectangle", rec_per)
+cir_area= 3.14*rad**2
+cir_circum= 2*3.14*rad
+
+print("Area of circle", cir_area)
+print("circumeference of circle", cir_circum)
