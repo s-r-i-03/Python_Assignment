90fc18c8e4f1871dd7431a230641a5bb7fa4d7e5
s-r-i-03 <145031870+s-r-i-03@users.noreply.github.com>
2024-10-08 20:22:14 +0530

    2.py

---
+++ Input Output and Arithmatic Operations/2.py
@@ -0,0 +1,9 @@
+dis=int(input("enter the distance in kms:"))
+met= dis * 1000
+print("distance in meters:" ,met)
+feet=dis * 3280.84
+print("distance in feet:",feet)
+inch=dis* 39370.1
+print("distance in inches:",inch)
+cm=dis* 100000
+print("distance in centimeters:",cm)
