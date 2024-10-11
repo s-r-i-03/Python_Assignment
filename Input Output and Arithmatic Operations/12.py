3284c12e265b9cb77d0ad4ee2bad0f9fadf165fe
s-r-i-03 <145031870+s-r-i-03@users.noreply.github.com>
2024-10-11 13:37:38 +0530

    #12

---
+++ Input Output and Arithmatic Operations/12.py
@@ -0,0 +1,4 @@
+sell_price=int(input("Enter the total selling price of 15 items:"))
+profit= int(input("Enter the total profit:"))
+single_cost= (sell_price-profit)/15
+print("The cost of one item is:",single_cost)
