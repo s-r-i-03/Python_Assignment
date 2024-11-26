d67779b591e68e423541ad20f632420ce0ddc43c
s-r-i-03 <145031870+s-r-i-03@users.noreply.github.com>
2024-11-26 19:16:26 +0530

    #2

---
+++ Condition Control/#2.py
@@ -0,0 +1,10 @@
+num=int(input("Enter the integer:"))
+
+if num == 0:
+    print(num,"is zero")    
+
+elif num%2==0:
+    print(num,"is even number")    
+else:
+    print(num,"is odd number")    
+
