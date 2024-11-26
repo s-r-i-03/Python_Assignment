d0fa4021f1641f2c44952c7665e0c6b8d0058472
s-r-i-03 <145031870+s-r-i-03@users.noreply.github.com>
2024-11-26 19:30:20 +0530

    #7

---
+++ Condition Control/#7.py
@@ -0,0 +1,7 @@
+ang1=float(input("Enter the value of angle 1:"))
+ang2=float(input("Enter the value of angle 2:"))
+ang3=float(input("Enter the value of angle 3:"))
+if(ang1+ang2+ang3==180):
+    print("It is a valid triangle")
+else:
+    print("It is not valid triangle")    
